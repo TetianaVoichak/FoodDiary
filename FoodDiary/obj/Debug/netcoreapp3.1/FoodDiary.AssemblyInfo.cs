205de828033bfd3c986aa76efa3742944948b469
Tetiana Voichak <tetiana.ozatska2012@gmail.com>
2025-03-13 11:09:42 +0100

    Save all changes before merging

--- FoodDiary/obj/Debug/netcoreapp3.1/FoodDiary.AssemblyInfo.cs
+++ FoodDiary/obj/Debug/netcoreapp3.1/FoodDiary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDiary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d385d607912434a099191e41b71dba1bcac5a056")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDiary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDiary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
